3fde2cf8b047c76904378df9bdf6100aebc0b6f5
Parker Moore <parkrmoore@gmail.com>
2014-04-23 17:58:57 -0400

    Use File.dirname(__FILE__) instead of __dir__

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'jekyll'
-require File.expand_path('../lib/jekyll-sitemap', __dir__)
+require File.expand_path('../lib/jekyll-sitemap', File.dirname(__FILE__))
 
 Jekyll.logger.log_level = 5
 
