2a543282cb8997d4a3b7fa989af4bea21b67ec29
Frank Taillandier <frank.taillandier@gmail.com>
2017-08-25 17:37:52 +0200

    Define path with __dir__ (#186)
    Merge pull request 186

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
-require 'jekyll'
-require File.expand_path('../lib/jekyll-sitemap', File.dirname(__FILE__))
+require "jekyll"
+require File.expand_path("../lib/jekyll-sitemap", __dir__)
 
 Jekyll.logger.log_level = :error
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.order = 'random'
+  config.order = "random"
 
-  SOURCE_DIR = File.expand_path("../fixtures", __FILE__)
-  DEST_DIR   = File.expand_path("../dest",     __FILE__)
+  SOURCE_DIR = File.expand_path("fixtures", __dir__)
+  DEST_DIR   = File.expand_path("dest", __dir__)
 
   def source_dir(*files)
     File.join(SOURCE_DIR, *files)
