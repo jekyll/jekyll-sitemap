18a6b096ab382a4179e09ec253371cb3fab3aac1
Pat Hawks <pat@pathawks.com>
2018-04-30 18:04:35 -0500

    Use Ruby 2.3 and Rubocop 0.55

--- lib/jekyll/page_without_a_file.rb
+++ lib/jekyll/page_without_a_file.rb
@@ -2,8 +2,10 @@
 
 module Jekyll
   class PageWithoutAFile < Page
+    # rubocop:disable Naming/MemoizedInstanceVariableName
     def read_yaml(*)
       @data ||= {}
     end
+    # rubocop:enable Naming/MemoizedInstanceVariableName
   end
 end
